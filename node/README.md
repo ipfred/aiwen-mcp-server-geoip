18c25e12bd13eb0e77189c1315fed91d503ac2bc
zhangfei <330446875>
2025-04-21 12:46:52 +0800

    +node version

---
+++ node/README.md
@@ -0,0 +1,87 @@
+# 埃文IP定位 MCP Server
+
+## 介绍
+[埃文科技](https://www.ipplus360.com/) 是全球IP地址高精准实时定位技术领航者 全球网络空间地图大数据服务提供商
+
+埃文科技IP定位API已全面兼容MCP协议
+
+MCP Server for the Aiwen IP Location API
+
+## 工具介绍
+
+1. IP定位 `aiwen_ip_location`
+- 描述：根据请求的IP地址，返回该IP地址的详细信息，包括国家、省份、城市、运营商、使用者、经纬度等。
+- 参数：
+  - `ip`：IP地址，必填 支持IPv4和IPv6
+
+- 输出示例
+
+```json
+{
+  "code": "Success",
+  "data": {
+    "continent": "亚洲",
+    "country": "中国",
+    "owner": "中国电信",
+    "isp": "中国电信",
+    "zipcode": "510000",
+    "timezone": "UTC+8",
+    "accuracy": "城市",
+    "source": "数据挖掘",
+    "areacode": "CN",
+    "adcode": "440100",
+    "asnumber": "4134",
+    "lat": "23.116548",
+    "lng": "113.295827",
+    "radius": "87.3469",
+    "prov": "广东省",
+    "city": "广州市"
+  },
+  "charge": true,
+  "msg": "查询成功",
+  "ip": "202.97.89.109",
+  "coordsys": "WGS84"
+}
+```
+
+
+## 快速使用
+
+### 获取API key
+通过aiwen官网 获取api key： https://mall.ipplus360.com/pros/IPVFourGeoAPI
+
+### MCP HOST中配置使用
+#### cursor
+```json
+{
+    "mcpServers": {
+        "aiwen-iplocation": {
+            "command": "npx",
+            "args": [
+                "-y",
+                "aiwen-mcp-server-geoip"
+            ],
+            "env": {
+                "AIWEN_API_KEY": "xxxxxx"
+            }
+        }
+    }
+}
+```
+#### vscode
+```json
+{
+    "mcpServers": {
+        "aiwen-iplocation": {
+            "command": "npx",
+            "args": [
+                "-y",
+                "aiwen-mcp-server-geoip"
+            ],
+            "env": {
+                "AIWEN_API_KEY": "xxxxxx"
+            }
+        }
+    }
+}
+```
---
+++ node/index.ts
@@ -0,0 +1,176 @@
+#!/usr/bin/env node
+
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import {
+  CallToolRequestSchema,
+  ListToolsRequestSchema,
+  Tool,
+} from "@modelcontextprotocol/sdk/types.js";
+import fetch from "node-fetch";
+
+// Response interfaces
+interface AiWenResponse {
+  code: number;
+  msg: string;
+  data?: string;
+}
+
+
+interface IPLocationResponse {
+  code: string;
+  data: {
+    continent: string;
+    country: string;
+    owner: string;
+    isp: string;
+    zipcode: string;
+    timezone: string;
+    accuracy: string;
+    source: string;
+    areacode: string;
+    adcode: string;
+    asnumber: string;
+    lat: string;
+    lng: string;
+    radius: string;
+    prov: string;
+    city: string;
+  };
+  charge: boolean;
+  msg: string;
+  ip: string;
+  coordsys: string;
+}
+
+
+function getApiKey(): string {
+    const apiKey = process.env.AIWEN_API_KEY;
+    if (!apiKey) {
+      console.error("AIWEN_API_KEY environment variable is not set");
+      process.exit(1);
+    }
+    return apiKey;
+  }
+
+const AIWEN_API_KEY = getApiKey();
+
+// Tool definitions
+
+
+const IP_LOCATION_TOOL: Tool = {
+  name: "aiwen_ip_location",
+  description: "IP定位 根据IP地址获取IP位置(定位到城市)、使用者、运营商、经纬度等信息 支持IPv4和IPv6",
+  inputSchema: {
+    type: "object",
+    properties: {
+      ip: {
+        type: "string",
+        description: "IP地址",
+      }
+    },
+    required: ["ip"],
+  }
+}
+
+
+const MAPS_TOOLS = [
+  IP_LOCATION_TOOL,
+] as const;
+
+const API_URL = "https://api.ipplus360.com/ip/geo/v1";
+
+// API handlers
+
+async function handleIPLocation(
+  ip: string,
+) {
+  const city_loc_url = `${API_URL}/city/`;
+  const url = new URL(city_loc_url);
+  url.searchParams.append("key", AIWEN_API_KEY);
+  url.searchParams.append("channel", "node_mcp");
+  url.searchParams.append("coordsys", "WGS84");
+  url.searchParams.append("ip", ip);
+
+  const response = await fetch(url.toString());
+  const data = await response.json() as IPLocationResponse;
+
+  if (data.code !== "Success") {
+    return {
+      content: [{
+        type: "text",
+        text: `IP address query failed: ${data.msg || data.code}`
+      }],
+      isError: true
+    }
+  }
+
+  return {
+    content: [{
+      type: "text",
+      text: JSON.stringify(data.data, null, 2)
+    }],
+    isError: false
+  }
+}
+
+
+// Server setup
+const server = new Server(
+  {
+    name: "mcp-server/aiwen_geoip",
+    version: "1.0.0",
+  },
+  {
+    capabilities: {
+      tools: {},
+    },
+  },
+);
+
+// Set up request handlers
+server.setRequestHandler(ListToolsRequestSchema, async () => ({
+  tools: MAPS_TOOLS,
+}));
+
+server.setRequestHandler(CallToolRequestSchema, async (request) => {
+  try {
+    switch (request.params.name) {
+      
+      case "aiwen_ip_location": {
+        const {ip} = request.params.arguments as {
+          ip: string;
+        };
+        return await handleIPLocation(ip);
+      }
+
+      default:
+        return {
+          content: [{
+            type: "text",
+            text: `Unknown tool: ${request.params.name}`
+          }],
+          isError: true
+        };
+    }
+  } catch (error) {
+    return {
+      content: [{
+        type: "text",
+        text: `Error: ${error instanceof Error ? error.message : String(error)}`
+      }],
+      isError: true
+    };
+  }
+});
+
+async function runServer() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error("Awien geoip MCP Server running on stdio");
+}
+
+runServer().catch((error) => {
+  console.error("Fatal error running server:", error);
+  process.exit(1);
+});
